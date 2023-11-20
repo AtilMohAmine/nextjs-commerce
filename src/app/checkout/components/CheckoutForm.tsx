96d3e7e28d8c939bef44f23aaaedc4ada779a8f5
Ataminos <mohamed.amine.mtb@gmail.com>
2023-11-20 20:23:21 +0100

    Add pay with card using Paypal

--- src/app/checkout/components/CheckoutForm.tsx
+++ src/app/checkout/components/CheckoutForm.tsx
@@ -138,85 +138,6 @@ export default function POST({ paypalClientId, subTotal, shipping }: Props) {
         <div className="px-3 md:w-5/12">
             <Bill subTotal={subTotal} shipping={shipping} />
             <div className="w-full mx-auto rounded-lg bg-white border border-gray-200 text-gray-800 font-light mb-6">
-                <div className="w-full p-3 border-b border-gray-200">
-                    <div className="mb-5">
-                            <img src="/cards.png" className="h-6 ml-1" />
-                    </div>
-                    <div>
-                        <div className="mb-3">
-                            <label className="text-gray-600 font-semibold text-sm mb-2 ml-1">Name on card</label>
-                            <div>
-                                <input className="w-full px-3 py-2 mb-1 border border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="John Smith" type="text" />
-                            </div>
-                        </div>
-                        <div className="mb-3">
-                            <label className="text-gray-600 font-semibold text-sm mb-2 ml-1">Card number</label>
-                            <div>
-                                <input className="w-full px-3 py-2 mb-1 border border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0000 0000 0000 0000" type="text" />
-                            </div>
-                        </div>
-                        <div className="mb-3 -mx-2 flex justify-center items-end">
-                            <div className="px-2 w-1/3">
-                                <label className="text-gray-600 font-semibold text-sm mb-2 ml-1">Expiration date</label>
-                                <div>
-                                    <select className="form-select w-full px-3 py-2 mb-1 border border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
-                                        <option value="01">01 - January</option>
-                                        <option value="02">02 - February</option>
-                                        <option value="03">03 - March</option>
-                                        <option value="04">04 - April</option>
-                                        <option value="05">05 - May</option>
-                                        <option value="06">06 - June</option>
-                                        <option value="07">07 - July</option>
-                                        <option value="08">08 - August</option>
-                                        <option value="09">09 - September</option>
-                                        <option value="10">10 - October</option>
-                                        <option value="11">11 - November</option>
-                                        <option value="12">12 - December</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="px-2 w-1/3">
-                                <select className="form-select w-full px-3 py-2 mb-1 border border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
-                                    <option value="2020">2020</option>
-                                    <option value="2021">2021</option>
-                                    <option value="2022">2022</option>
-                                    <option value="2023">2023</option>
-                                    <option value="2024">2024</option>
-                                    <option value="2025">2025</option>
-                                    <option value="2026">2026</option>
-                                    <option value="2027">2027</option>
-                                    <option value="2028">2028</option>
-                                    <option value="2029">2029</option>
-                                </select>
-                            </div>
-                            <div className="px-2 w-1/3">
-                                <label className="text-gray-600 font-semibold text-sm mb-2 ml-1">Security code</label>
-                                <div>
-                                    <input className="w-full px-3 py-2 mb-1 border border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="000" type="text" />
-                                </div>
-                            </div>
-                        </div>
-                        <button 
-                                className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 w-full"
-                                onClick={() => {
-                                    startTransition(async () => { 
-                                        await checkout(
-                                            emailInput.current.value,
-                                            firstNameInput.current.value,
-                                            lastNameInput.current.value,
-                                            addressInput.current.value,
-                                            cityInput.current.value,
-                                            countryInput.current.value,
-                                            provinceInput.current.value,
-                                            postalCodeInput.current.value,
-                                            phoneInput.current.value
-                                        )
-                                    })
-                                }}>
-                            Pay Now
-                        </button>
-                    </div>
-                </div>
                 <div className="w-full p-3">
                 <PayPalScriptProvider
                     options={{
@@ -230,7 +151,6 @@ export default function POST({ paypalClientId, subTotal, shipping }: Props) {
                             label: 'pay',
                             height: 50,
                         }}
-                        fundingSource={FUNDING.PAYPAL}
                         createOrder={async (data, actions) => {
                             let order_id = await paypalCreateOrder()
                             return order_id + ''
